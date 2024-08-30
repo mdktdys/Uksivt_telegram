3df020b8fe4349c5764ecc7487e0d6890d3c205c
EventGamer67 <islamoffdanil67@gmail.com>
2024-08-30 17:00:11 +0500

    fix

--- utils/extensions.py
+++ utils/extensions.py
@@ -15,9 +15,10 @@ def month_name(date: datetime.datetime) -> str:
 
 
 def week_number_from_september(date: datetime.datetime) -> int:
-    first_september = datetime.date(2024, 9, 1)
-    if date.date() < first_september:
-        return 0
-    days_difference = (date - first_september).days
-    week_number = days_difference // 7 + 1
-    return week_number
+    return 1
+    # first_september = datetime.date(2024, 9, 1)
+    # if date.date() < first_september:
+    #     return 0
+    # days_difference = (date - first_september).days
+    # week_number = days_difference // 7 + 1
+    # return week_number
