3ad439e8e88b2f68db338fd2538dc5c178358b0d
EventGamer67 <islamoffdanil67@gmail.com>
2024-09-03 11:40:42 +0500

    set keys to env

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import asyncio
 import logging
 import sys
+import os
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 from callbacks import navigation, search
 
-TOKEN = "7283968288:AAEdrChiKr149d2p5zeoVZ29bWEJIFlDtTs"
-API_URL = "https://api.uksivt.xyz/api/v1/"
+TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
+API_URL = os.environ.get("TELEGRAM_API_URL")
 
 
 async def main() -> None:
