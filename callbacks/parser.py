20e9ba47f0e2c5f67575e337055c4744b0b243d9
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-04 00:41:06 +0500

    env

--- callbacks/parser.py
+++ callbacks/parser.py
@@ -1,38 +1,23 @@
-import asyncio
 import datetime
 import html
 import traceback
 from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
-from celery.result import AsyncResult
 
 router = Router()
 
 admins = [1283168392]
-
-from your_celery_app import get_latest_zamena_link  # Импортируйте вашу задачу
-import asyncio
 
 
 @router.message(F.text, Command("latest"))
 async def my_handler(message: Message):
     try:
         chat_id = message.chat.id
-        # Отправляем задачу с использованием delay()
-        task = get_latest_zamena_link.delay()  # Вызываем delay() на задаче
-
-        # Функция для периодической проверки статуса задачи
-        async def check_task_result(task_id):
-            result = AsyncResult(task_id)  # Создаем AsyncResult для проверки
-            while not result.ready():
-                await asyncio.sleep(1)  # Ждем 1 секунду перед следующей проверкой
-            return result.result  # Возвращаем результат, когда задача завершена
-
-        # Запускаем проверку в отдельной задаче
-        task_result = await check_task_result(task.id)
-
-        await message.answer(f"{task_result}")
+        # Lazy import inside the function
+        from telegram import telegram_celery_app
+        telegram_celery_app.send_task("parser.tasks.get_latest_zamena_link_telegram",args=[chat_id])
+        await message.answer(f"Отправлено в очередь")
     except Exception as e:
         error_body = f"{str(e)}\n\n{traceback.format_exc()}"
         from utils.sender import send_error_message
@@ -44,5 +29,4 @@ async def my_handler(message: Message):
             time_=datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S %p"),
             error_body=error_body,
         )
-
 
