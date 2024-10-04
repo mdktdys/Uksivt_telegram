9230cc0d4f59d36e864fa4f05c7c95e180a85752
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-04 15:00:12 +0500

    test

--- telegram/tasks.py
+++ telegram/tasks.py
@@ -2,14 +2,15 @@ import asyncio
 import functools
 from telegram_celery import telegram_celery_app
 from utils.sender import send_message
+import celery_pool_asyncio
 
 
-def sync(f):
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        return asyncio.get_event_loop().run_until_complete(f(*args, **kwargs))
-
-    return wrapper
+# def sync(f):
+#     @functools.wraps(f)
+#     def wrapper(*args, **kwargs):
+#         return asyncio.get_event_loop().run_until_complete(f(*args, **kwargs))
+#
+#     return wrapper
 
 
 @telegram_celery_app.task
--- telegram_celery.py
+++ telegram_celery.py
@@ -1,14 +1,15 @@
 from celery import Celery
 
 from secrets import BROKER_URL, BACKEND_URL
+import celery_pool_asyncio
 
 telegram_celery_app = Celery(
-    'telegram_bot',
+    "telegram_bot",
     broker=BROKER_URL,
     backend=BACKEND_URL,
 )
 
-telegram_celery_app.autodiscover_tasks(packages=["telegram"],force=True)
+telegram_celery_app.autodiscover_tasks(packages=["telegram"], force=True)
 
 print("***")
 print(telegram_celery_app.tasks.keys())
