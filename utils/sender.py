a1cca9c955aaec722961a056585a6eb7efd247b5
EventGamer67 <islamoffdanil67@gmail.com>
2024-10-04 13:01:33 +0500

    test

--- utils/sender.py
+++ utils/sender.py
@@ -1,18 +1,36 @@
+import datetime
 import html
 from aiogram import Bot
 from main import bot
+from secrets import DEBUG_CHANNEL
 
 
-async def send_message(chat_id, message):
-    await bot.send_message(chat_id=str(chat_id), text=str(message))
+async def send_message(chat_id, message: str):
+    try:
+        await bot.send_message(chat_id=str(chat_id), text=str(message))
+    except Exception as e:
+        await send_error_message(
+            bot=bot,
+            chat_id=DEBUG_CHANNEL,
+            error_header="Ошибка",
+            application="Kronos",
+            time_=str(datetime.datetime.now()),
+            error_body=str(e),
+        )
 
 
-async def send_error_message(bot: Bot, chat_id: int, error_header: str, application: str,
-                             time_: str, error_body: str):
+async def send_error_message(
+    bot: Bot,
+    chat_id: int,
+    error_header: str,
+    application: str,
+    time_: str,
+    error_body: str,
+):
     message = (
         f"<b>⚠️ {error_header}</b>\n"
         f"Application: {application}\n"
         f"Time: {time_}\n\n"
         f"<pre>{html.escape(error_body[:3000])}</pre>"
     )
-    await bot.send_message(chat_id=chat_id, text=message, parse_mode='html')
+    await bot.send_message(chat_id=chat_id, text=message, parse_mode="html")
