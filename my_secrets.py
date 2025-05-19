876d759cb6cacddbf5796d3104501e8bd1e49ace
mdktdys <islamoffdanil67@gmail.com>
2025-05-20 00:48:09 +0500

    a

--- my_secrets.py
+++ my_secrets.py
@@ -1,5 +1,5 @@
 import os
-LOG_LEVEL = "CRITICAL"
+LOG_LEVEL = 1
 
 MAIN_CHANNEL_ANCHOR_MESSAGE: int = int(os.environ["TELEGRAM_MAIN_CHANNEL_ANCHOR_MESSAGE"])
 API_URL: str = os.environ.get("TELEGRAM_API_URL") or "http://127.0.0.1:8000/api/v1/"
