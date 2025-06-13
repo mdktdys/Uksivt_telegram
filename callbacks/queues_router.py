81c7396173822518fdd8f00415195caa3a64ded4
mdktdys <islamoffdanil67@gmail.com>
2025-06-13 15:53:55 +0500

    fix

--- callbacks/queues_router.py
+++ callbacks/queues_router.py
@@ -49,7 +49,7 @@ async def show_queue(callback: CallbackQuery, api: ScheduleApi) -> None:
     text: str = f'''
 Очередь {queue.name}
 
-f{'\n'.join(lines)}
+{'\n'.join(lines)}
 '''
     await callback.bot.edit_message_text(
         chat_id = callback.message.chat.id,
