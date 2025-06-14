7121d37ccfc2081daeb330245717d6266dcb27c1
mdktdys <islamoffdanil67@gmail.com>
2025-06-14 13:03:32 +0500

    start

--- callbacks/menu_router.py
+++ callbacks/menu_router.py
@@ -7,4 +7,8 @@ router = Router(name = 'menu_router')
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    message.answer(text = 'Для поиска расписания начните вводить\n@UksivtZameny_bot [Наименование]', reply_markup = menu_screen_keyboard())
+    await message.bot.send_message(
+        chat_id = message.chat.id,
+        text = 'Для поиска расписания начните вводить\n@UksivtZameny_bot [Наименование]',
+        reply_markup = menu_screen_keyboard()
+    )
