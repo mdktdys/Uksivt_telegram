e35a32a3401c8865cb6a901a7d58e38fe5c6e7fe
mdktdys <islamoffdanil67@gmail.com>
2025-08-25 00:32:22 +0500

    fix

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,6 +1,7 @@
 import httpx
-from my_secrets import API_KEY
+
 from models.user_model import User
+from my_secrets import API_KEY
 
 
 class UserService:
@@ -13,7 +14,7 @@ class UserService:
 
 
     async def get_user(self, user_id: int) -> User | None:
-        url: str = f"{self.base_url}/api/v2/users/telegram"
+        url: str = f"{self.base_url}/v2/users/telegram"
 
         response: httpx.Response = await self.client.get(url, headers=self.headers, params={"user_id": user_id})
         if response.status_code == 200:
